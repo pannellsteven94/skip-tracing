e30e1f8a656d3501691eba17f0ac36152772a32a
pannellsteven94 <pannell.steven94@gmail.com>
2024-11-27 16:19:14 -0500

    update

--- server.js
+++ server.js
@@ -60,5 +60,5 @@ app.post('/property', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
